85132d22d96c99ddf8640500536bfdda66309cb4
Swapnil Ahirwar <ahirwarswapnil1@gmail.com>
2023-03-14 23:35:32 +0530

    Setting the server end

--- frontend/src/components/ToDoForm.jsx
+++ frontend/src/components/ToDoForm.jsx
@@ -1,11 +1,21 @@
 import './ToDoForm.css';
+import { useState } from 'react';
 
 const ToDoForm = () => {
 
+    const [text, setText] = useState("");
+
+    const onFormSubmit = () => {
+
+    }
+
+    const onInputChange = (e) => {
+        setText(e.target.value);
+    }
 
     return (
-        <form className='form'>
-            <input placeholder='Enter new ToDo...'  />
+        <form className='form' onSubmit={onFormSubmit}>
+            <input placeholder='Enter new ToDo...' onChange={onInputChange} />
         </form>
     );
 }
